8a3b76b1c09e9bc3e2054eaa91f6f10745cfb285
mirkamranmahshid <secure.communicatione@gmail.com>
2025-03-12 07:12:32 +0330

    initial commit

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -5,7 +5,7 @@ import Projects from "@/components/main/Projects";
 import Skills from "@/components/main/Skills";
 //import Image from "next/image";
 import {useTranslations} from 'next-intl';
-export default function Home({locale}:{ locale: string }) { setRequestLocale(locale);  
+export default function Home({locale}:{ locale: any }) { setRequestLocale(locale);  
       const t = useTranslations('Home');
   return ( 
     <main className="h-full w-full">
